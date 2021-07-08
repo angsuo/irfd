84c45788066c3f0d8344b217cedce79fbbbaa875
an9513 <angsuo91@gmail.com>
2021-07-08 19:14:38 +0200

    Documented the code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
-import express from 'express';
+// Import modules
+//      Express
+const express = require("express");
 
+// Declare variables | Read environment variables
+//      Port
 const PORT = process.env.PORT || 3000
 
+// Instantiate the modules
+//      Express
 const app = express();
 
+// Server Methods
+//      Response to Get request at "/" (Basic URL request)
 app.get("/", (_, res) => {
     res.status(200).json("Hello from node server with Typescript")
 })
 
+// Start server
 app.listen(PORT, () => console.log(`server running at http://localhost:${PORT}`));
