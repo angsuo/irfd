f50d8632981f89088f8d4e107649bd337dbb77c3
an9513 <angsuo91@gmail.com>
2021-07-08 19:08:02 +0200

    Added express with types, simple get '/' server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world");
+import express from 'express';
+
+const PORT = process.env.PORT || 3000
+
+const app = express();
+
+app.get("/", (_, res) => {
+    res.status(200).json("Hello from node server with Typescript")
+})
+
+app.listen(PORT, () => console.log(`server running at http://localhost:${PORT}`));
