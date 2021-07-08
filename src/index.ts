93e91a2d263bb8443fab5a6f986fdac51a6de739
an9513 <angsuo91@gmail.com>
2021-07-08 19:57:58 +0200

    played around the js arrays reduce method

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // Import modules
 //      Express
-const express = require("express");
+import express from "express";
 
 // Declare variables | Read environment variables
 //      Port
@@ -10,10 +10,32 @@ const PORT = process.env.PORT || 3000
 //      Express
 const app = express();
 
-// Server Methods
-//      Response to Get request at "/" (Basic URL request)
+// API Routes
+//      Get "/" (Basic URL request)
 app.get("/", (_, res) => {
-    res.status(200).json("Hello from node server with Typescript")
+
+    // Send the response from server
+    res.send("<h1>Internet Restaurant and Food Database</h1>")
+})
+//      Get "/restaurants"
+//          Send a list of 10 restaurants
+app.get("/restaurants", (req, res) => {
+    // Get informations from the request (later the GPS coordinates)
+    const reqInfos = req.headers
+    console.log(`New request from browser : ${reqInfos["user-agent"]}`);
+
+    /**
+     * 10 restaurants near Request GPS position
+     */
+    const restaurants = ["resto 1", "resto 2", "resto 3", "resto 4", "resto 5", "resto 6", "resto 7", "resto 8", "resto 9", "resto 10"];
+
+    /**
+     * 10 restaurant names enclosed in <p> tag
+     */
+    const restaurantNames = restaurants.reduce((prev, curr, i) => `${i === 1 ? "<p>" + prev + "</p>" : prev}<p>${curr}</p>`)
+
+    // Send response back
+    res.send(restaurantNames);
 })
 
 // Start server
