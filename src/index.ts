f6122a6b6326a927168f40143dfec9d9b74f83a4
an9513 <angsuo91@gmail.com>
2021-07-09 17:53:39 +0200

    added packages for orm and ormconfig file with dummy IDs

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import express from "express";
 
 // Declare variables | Read environment variables
 //      Port
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 // Instantiate the modules
 //      Express
@@ -13,25 +13,33 @@ const app = express();
 // API Routes
 //      Get "/" (Basic URL request)
 app.get("/", (_, res) => {
-
-    // Send the response from server
-    res.json("Welcome to the Internet Restaurants and Food Database")
-})
+  // Send the response from server
+  res.json("Welcome to the Internet Restaurants and Food Database");
+});
 //      Get "/restaurants"
 //          Send an array of 10 restaurants objects
 app.get("/restaurants", (req, res) => {
-    // Get informations from the request (later the GPS coordinates)
-    const agent = req.headers["user-agent"];
-    console.log(`New request for restaurants from browser : ${agent}`);
+  // Get informations from the request (later the GPS coordinates)
+  const agent = req.headers["user-agent"];
+  console.log(`New request for restaurants from browser : ${agent}`);
 
-    /**
-     * 10 restaurants near Request GPS position
-     */
-    const restaurants = [{name:"resto 1"}, {name:"resto 2"}, {name:"resto 3"}, {name:"resto 4"}, {name:"resto 5"}, {name:"resto 6"}];
+  /**
+   * 10 restaurants near Request GPS position
+   */
+  const restaurants = [
+    { name: "resto 1" },
+    { name: "resto 2" },
+    { name: "resto 3" },
+    { name: "resto 4" },
+    { name: "resto 5" },
+    { name: "resto 6" },
+  ];
 
-    // Send JSON response back
-    res.json(restaurants);
-})
+  // Send JSON response back
+  res.json(restaurants);
+});
 
 // Start server
-app.listen(PORT, () => console.log(`server running at http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`server running at http://localhost:${PORT}`)
+);
