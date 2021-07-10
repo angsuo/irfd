baf7ee55ead0d4cef3ff80a96efb3edf0f4b08fe
an9513 <angsuo91@gmail.com>
2021-07-10 09:31:38 +0200

    Typeorm connection IDs should read environment variables instead of vulnerable plain text

---
+++ ormconfig.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const { DB_TYPE, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } =
+  process.env;
+
+module.exports = {
+  type: DB_TYPE,
+  host: DB_HOST,
+  port: DB_PORT,
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+  synchronize: true,
+  logging: false,
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscriber/**/*.ts"],
+};
